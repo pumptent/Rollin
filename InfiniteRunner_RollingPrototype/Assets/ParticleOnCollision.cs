fd1f7f173329641cf4f30519394cdb573f8f6ca1
Jonathan Schonfeld <jerrycritic@gmail.com>
2016-11-28 20:36:55 -0500

    fifthBuild
    fifthBuild

---
+++ InfiniteRunner_RollingPrototype/Assets/ParticleOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ParticleOnCollision : MonoBehaviour{
+
+	public ParticleSystem _psystem;
+
+	void Awake() {
+		_psystem = GetComponent<ParticleSystem> ();
+	}
+
+	void OnTriggerEnter (Collider col)  {
+
+
+		_psystem.Play();
+
+	}
+
+}
